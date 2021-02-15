7e096ad60c4cbf82e202993e58542c9289e95fca
drewkiimon <andrew.r.pagan@gmail.com>
2021-02-15 08:22:50 -0800

    scaffolding for blog

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,28 @@
+import Head from "next/head";
+import { attributes, react as HomeContent } from "../../content/home.md";
+
 const Blog = () => {
-	return <div>Blog :)</div>;
+	let { title, cats } = attributes;
+
+	return (
+		<>
+			<Head>
+				<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+			</Head>
+			<article>
+				<h1>{title}</h1>
+				<HomeContent />
+				<ul>
+					{cats.map((cat, k) => (
+						<li key={k}>
+							<h2>{cat.name}</h2>
+							<p>{cat.description}</p>
+						</li>
+					))}
+				</ul>
+			</article>
+		</>
+	);
 };
 
 export default Blog;
