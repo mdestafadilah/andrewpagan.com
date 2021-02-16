a942f69339b0497dad95819d6eda8a506b8719c7
drewkiimon <andrew.r.pagan@gmail.com>
2021-02-15 16:48:03 -0800

    404 giphy

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,20 @@
 import Axios from "axios";
 import { useEffect, useState } from "react";
+import Head from "next/head";
 
 import Navigation from "./Navigation";
+import Footer from "./Footer";
 
 const { NEXT_PUBLIC_GIPHY_API_KEY } = process.env;
 import styles from "./404.module.scss";
 
+const getRandomInt = (max) => {
+	return Math.floor(Math.random() * Math.floor(max));
+};
+
 const fourOhFour = () => {
+	const MAX_GIPHY_SEARCH = 4999;
+
 	const [gifs, setGifs] = useState([]);
 	const [offset, setOffset] = useState(0);
 
@@ -19,7 +27,7 @@ const fourOhFour = () => {
 					params: {
 						api_key: NEXT_PUBLIC_GIPHY_API_KEY,
 						limit: 1,
-						offset,
+						offset: getRandomInt(MAX_GIPHY_SEARCH),
 					},
 				});
 
@@ -28,17 +36,33 @@ const fourOhFour = () => {
 		};
 
 		getGif();
-	}, []);
+	}, [offset]);
+
+	const getRandomTrendingGIF = () => {
+		let randomNumber = getRandomInt(MAX_GIPHY_SEARCH);
+
+		setOffset(randomNumber);
+	};
 
 	return (
-		<div>
+		<>
+			<Head>
+				<title>404 - you okay?</title>
+			</Head>
 			<Navigation />
+
 			<div className={styles["gif-holder"]}>
 				{gifs.map((gif) => (
-					<img key={gif.id} src={gif.images.downsized.url} />
+					<img
+						key={gif.id}
+						src={gif.images.downsized.url}
+						onClick={getRandomTrendingGIF}
+					/>
 				))}
 			</div>
-		</div>
+
+			<Footer />
+		</>
 	);
 };
 
