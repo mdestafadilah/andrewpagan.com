295bb94f54e0f03a3c4b5c989fda554e68f92a7e
drewkiimon <andrew.r.pagan@gmail.com>
2021-02-15 16:54:07 -0800

    link for going back to homepage

--- pages/Navigation.js
+++ pages/Navigation.js
@@ -1,16 +1,20 @@
 import styles from "./Navigation.module.scss";
+import Link from "next/link";
+
 const LOGO = "../images/AP.png";
 
 const Navigation = () => {
 	return (
 		<nav className={styles.navigation}>
-			<img
-				className={styles.icon}
-				src={LOGO}
-				height="72"
-				width="72"
-				alt="Andrew Pagan the Third logo"
-			/>
+			<Link href="/">
+				<img
+					className={styles.icon}
+					src={LOGO}
+					height="72"
+					width="72"
+					alt="Andrew Pagan the Third logo"
+				/>
+			</Link>
 		</nav>
 	);
 };
