ed0f31e119e4164a32cf4418087870bdd6d2b807
drewkiimon <andrew.r.pagan@gmail.com>
2021-02-27 16:59:32 -0800

    Add icons to links

--- pages/links/index.tsx
+++ pages/links/index.tsx
@@ -1,26 +1,61 @@
 import React from "react";
 import { links } from "./../../public/quick-link.json";
 import styles from "./links.module.scss";
-import Image from "next/image";
+// import Image from "next/image";
+import {
+	ImInstagram,
+	ImSoundcloud2,
+	ImTwitch,
+	ImTwitter,
+	ImYoutube,
+} from "react-icons/im";
 
-const profileImage = "/images/andrew-pagan.jpg";
+// const profileImage = "/images/andrew-pagan.jpg";
 
-const Links: React.FC = () => {
+interface ListItemProp {
+	url: string;
+	name: string;
+}
+
+const ListItem: React.FC<ListItemProp> = ({ url, name }) => {
 	const openLinkInNewTab = (link: string) => {
 		window.open(link, "_blank");
 	};
 
+	const iconPicker = () => {
+		switch (name) {
+			case "SoundCloud":
+				return <ImSoundcloud2 />;
+			case "Instagram":
+				return <ImInstagram />;
+			case "Twitter (personal)":
+				return <ImTwitter />;
+			case "Twitter (professional)":
+				return <ImTwitter />;
+			case "Youtube":
+				return <ImYoutube />;
+			case "Twitch":
+				return <ImTwitch />;
+		}
+	};
+
+	return (
+		<li
+			className={styles.link}
+			key={url}
+			onClick={() => openLinkInNewTab(url)}
+		>
+			{iconPicker()} {name}
+		</li>
+	);
+};
+
+const Links: React.FC = () => {
 	return (
 		<div className={styles.links}>
 			<ul>
 				{links.map((link) => (
-					<li
-						className={styles.link}
-						key={link.url}
-						onClick={() => openLinkInNewTab(link.url)}
-					>
-						{link.name}
-					</li>
+					<ListItem url={link.url} name={link.name} />
 				))}
 			</ul>
 		</div>
