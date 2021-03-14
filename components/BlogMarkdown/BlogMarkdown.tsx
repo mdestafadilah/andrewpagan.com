ed667e933bd78be1a17ea92b4239569ad0020d54
drewkiimon <andrew.r.pagan@gmail.com>
2021-03-14 10:35:49 -0700

    A lot of CSS changes

--- components/BlogMarkdown/BlogMarkdown.tsx
+++ components/BlogMarkdown/BlogMarkdown.tsx
@@ -12,52 +12,30 @@ interface ChildrenProps {
 
 const CustomLink = ({ as, href, ...otherProps }: CustomLinkProps) => {
   return (
-    <>
-      <Link as={as} href={href}>
-        <a className={styles['custom-link']} {...otherProps} />
-      </Link>
-    </>
+    <Link as={as} href={href}>
+      <a className={styles['custom-link']} {...otherProps} />
+    </Link>
   );
 };
 
 const CustomH1 = ({ children }: ChildrenProps) => {
-  return (
-    <>
-      <h1 className={styles['custom-h1']}>{children}</h1>
-    </>
-  );
+  return <h1 className={styles['custom-h1']}>{children}</h1>;
 };
 
 const CustomH2 = ({ children }: ChildrenProps) => {
-  return (
-    <>
-      <h2 className={styles['custom-h2']}>{children}</h2>
-    </>
-  );
+  return <h2 className={styles['custom-h2']}>{children}</h2>;
 };
 
 const CustomP = ({ children }: ChildrenProps) => {
-  return (
-    <>
-      <p className={styles['custom-p']}>{children}</p>
-    </>
-  );
+  return <p className={styles['custom-p']}>{children}</p>;
 };
 
 const CustomStrong = ({ children }: ChildrenProps) => {
-  return (
-    <>
-      <span className={styles['custom-strong']}>{children}</span>
-    </>
-  );
+  return <span className={styles['custom-strong']}>{children}</span>;
 };
 
 const CustomUl = ({ children }: ChildrenProps) => {
-  return (
-    <>
-      <ul className={styles['custom-ul']}>{children}</ul>
-    </>
-  );
+  return <ul className={styles['custom-ul']}>{children}</ul>;
 };
 
 export { CustomLink, CustomH1, CustomH2, CustomP, CustomStrong, CustomUl };
