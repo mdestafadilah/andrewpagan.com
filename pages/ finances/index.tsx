11fc9e37b7c1018a40f2185dbd371b26b5f48627
drewkiimon <andrew.r.pagan@gmail.com>
2022-01-22 22:25:46 -0800

    Added highcharts

--- pages/ finances/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type Props = {};
-
-const Finances = (props: Props) => {
-  return <div>Finances</div>;
-};
-
-export default Finances;
---
+++ pages/finances.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import * as Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+type Props = {};
+
+const options: Highcharts.Options = {
+  chart: {
+    type: 'column',
+  },
+  title: {
+    text: 'Stuff',
+  },
+  xAxis: {
+    categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas'],
+  },
+  yAxis: {
+    min: 0,
+    title: {
+      text: 'Total fruit consumption',
+    },
+    stackLabels: {
+      enabled: true,
+      style: {
+        fontWeight: 'bold',
+        color: 'gray',
+      },
+    },
+  },
+  legend: {
+    align: 'right',
+    x: -30,
+    verticalAlign: 'top',
+    y: 25,
+    floating: true,
+    backgroundColor: 'white',
+    borderColor: '#CCC',
+    borderWidth: 1,
+    shadow: false,
+  },
+  tooltip: {
+    headerFormat: '<b>{point.x}</b><br/>',
+    pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}',
+  },
+  plotOptions: {
+    column: {
+      stacking: 'normal',
+      dataLabels: {
+        enabled: true,
+      },
+    },
+  },
+  series: [
+    {
+      name: 'John',
+      data: [5, 3, 4, 7, 2],
+    },
+    {
+      name: 'Jane',
+      data: [2, 2, 3, 2, 1],
+    },
+    {
+      name: 'Joe',
+      data: [3, 4, 4, 2, 5],
+    },
+  ],
+};
+
+const Finances = (props: Props) => {
+  const chartComponentRef = React.useRef<HighchartsReact.RefObject>(null);
+
+  return (
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={options}
+      ref={chartComponentRef}
+      {...props}
+    />
+  );
+};
+
+export default Finances;
